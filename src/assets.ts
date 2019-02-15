dc8aedc83ffd809c7c5d8326a9ecca79f19c4fa6
Senya Pugach <upisfree@artlebedev.ru>
2019-02-15 07:10:12 +0300

    destructible objects, particles after destruction

--- src/assets.ts
+++ src/assets.ts
@@ -2,7 +2,10 @@ const ASSETS = {
   IMAGES: {
     'player': 'assets/player.png',
     'vasya': 'assets/test.jpg',
-    'drone': 'assets/drone.png'
+    'drone': 'assets/drone.png',
+    'wall-100': 'assets/wall/wall-100.png',
+    'wall-50': 'assets/wall/wall-50.png',
+    'wall-destruction-particle': 'assets/wall/wall-destruction-particle.png',
   },
   SOUNDS: []
 }
--- src/level.ts
+++ src/level.ts
@@ -3,20 +3,17 @@ import Wall from './enity/wall';
 import WallStack from './enity/wall-stack';
 import Slingshot from './enity/slingshot';
 const PhaserMatterCollisionPlugin = (<any>window).PhaserMatterCollisionPlugin;
-const { Body, Bodies, Vector } = Phaser.Physics.Matter.Matter;
+const { Body, Bodies } = Phaser.Physics.Matter.Matter;
 
 export default class Level {
   constructor(scene) {
     this.scene = scene;
 
-    // оптимизация
-    // let walls = [];
-
     let slingshot = new Slingshot(this.scene, 300, 700);
 
     let ground = new Wall(this.scene, 900, 900, 800, 50, { isStatic: true, friction: 0.75 });
     
-    let cube = new Wall(this.scene, 900, 400, 50, 200, {
+    let wall1 = new Wall(this.scene, 900, 700, 50, 200, {
       density: 0.005,
       friction: 0.001,
       label: 'wall'
@@ -25,55 +22,32 @@ export default class Level {
       // frictionAir: 0
     });
 
-    // let wallStack = new WallStack(this.scene, 900, 100, 50, 50, 5, 5);
-
-
-    // let wall3 = new Wall(this, 300, 0, 100, 100);
-
-    // walls.push(wall1, wall2, wall3);
-
-    this.scene.matterCollision.addOnCollideStart({
-      objectA: cube.sprite,
-      // objectB: ground.sprite,
-      callback: function(eventData) {
-        const { bodyA, bodyB, gameObjectA, gameObjectB, pair } = eventData;
-
-        // console.log(bodyA.mass, bodyB.mass);
-        let bodyMass;
-
-        if (bodyB.mass === Infinity) {
-          bodyMass = 1;
-        } else {
-          bodyMass = bodyB.mass;
-        }
-
-        var bodyAMomentum = Vector.mult(bodyA.velocity, bodyA.mass);
-        var bodyBMomentum = Vector.mult(bodyB.velocity, bodyMass);
-        var relativeMomentum = Vector.sub(bodyAMomentum, bodyBMomentum);
-
-        // это сила импульса
-        console.log(Vector.magnitude(relativeMomentum));  
-
-        // if (Vector.magnitude(relativeMomentum) > threshold) {
-        //     // do something
-        // }
-
-        // var bodyAMomentum = Vector.mult(bodyAVelocity, bodyA.mass);
-        // var bodyBMomentum = Vector.mult(bodyBVelocity, bodyB.mass);
-        // var relativeMomentum = Vector.sub(bodyAMomentum, bodyBMomentum);
-
-        // if (Vector.magnitude(relativeMomentum) > threshold) {
-        //     // do something
-        // }
-
-        // bodyA & bodyB are the Matter bodies of the player and door respectively
-        // gameObjectA & gameObjectB are the player and door respectively
-        // pair is the raw Matter pair data
-      },
-      context: this
+    let wall2 = new Wall(this.scene, 1100, 700, 50, 200, {
+      density: 0.005,
+      friction: 0.001,
+      label: 'wall'
+      // friction: 0.75,
+      // restitution: 0,
+      // frictionAir: 0
     });
 
-    // this.matter.add.image(400, 550, 'platform', null, { isStatic: true });
+    let wall3 = new Wall(this.scene, 700, 700, 50, 200, {
+      density: 0.005,
+      friction: 0.001,
+      label: 'wall'
+      // friction: 0.75,
+      // restitution: 0,
+      // frictionAir: 0
+    });
+
+    let wall4 = new Wall(this.scene, 500, 700, 50, 200, {
+      density: 0.005,
+      friction: 0.001,
+      label: 'wall'
+      // friction: 0.75,
+      // restitution: 0,
+      // frictionAir: 0
+    });
   }
 
   scene: any;
